d59b7bdda991fa68d5419717ed630cd3d8656c97
VadimStarton <sedoigamer@gmail.com>
2023-09-20 13:35:27 +0300

    first commit

---
+++ Front-end/js-intro/kintamieji/number.js
@@ -0,0 +1,39 @@
+/*
+Number
+
+-teigiami / neigiami
+-sveikieji / desimtainiai
+-normalus / nenormalus
+
+Iniciavimo budas:
+- const (default)
+-let (jei kinta informacija)
+var (niekada)
+*/
+
+const amzius = 99;
+let pinigine = 0;
+var minusas = -7.56;
+
+console.log(amzius);
+console.log(pinigine);
+console.log(minusas);
+
+const x1 = 1;
+const x2 = 2;
+const x3 = 3;
+const x4 = 4;
+const x5 = 5;
+
+const n1 = 7;
+const n2 = 5;
+
+const sum = n1 + n2;
+console.log(sum);
+
+const div = n1 / n2
+console.log(div)
+
+const complex = 2 + 2 * 2;
+console.log(complex)
+
