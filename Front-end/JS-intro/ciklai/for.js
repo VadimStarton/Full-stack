535498ed0b52c5914cda7228eb839a10bf050ea3
VadimStarton <sedoigamer@gmail.com>
2023-10-13 12:16:56 +0300

    Atstatymas for

--- Front-end/JS-intro/ciklai/for.js
+++ Front-end/JS-intro/ciklai/for.js
@@ -1,20 +1,20 @@
-/* 
-FOR - ciklai
-
+/*
+FOR - "for" ciklas
+bazinis is visu galimu ciklu
 */
 
 console.clear();
 
-console.log ('START');
+console.log('START');
 
-// intervalas; [0, 5]
+// intervalas: [0, 5)
 for (let i = 0; i < 5; i++) {
     console.log(i, 'pirmas');
 }
 
 console.log('PAUSE');
 
-//intervalas [-2, 7]
+// intervalas: [-2, 7]
 for (let i = -2; i <= 7; i++) {
     console.log(i, 'antras');
 }
@@ -24,37 +24,37 @@ console.log('FINISH');
 console.clear();
 
 for (let f = 0; f < 7; f++) {
-    const sentence = `The number is: ${f + 1},`;
+    const sentence = `The number is: ${f + 1}.`;
     console.log(sentence);
 }
 
 console.clear();
 
-//skaiciai intervale nuo 3 iki 8 imtinai yra: 3 4 5 6 7 8 
-
+// Skaiciai intervale nuo 3 iki 8 imtinai yra: 3 4 5 6 7 8.
 
 const start = 3;
 const finish = 8;
+let numberSequence = '';
 
-let numberSeguence = ''; 
-
-for(let i = start; i <= finish; i++) {
-        numberSeguence += ' ' + i;
+for (let i = start; i <= finish; i++) {
+    numberSequence += ' ' + i;
 }
 
-const numberLine = `Skaiciai intervale nuo ${start} iki ${finish} imtinai yra:${numberSeguence}.`;
+const numberLine = `Skaiciai intervale nuo ${start} iki ${finish} imtinai yra:${numberSequence}.`;
+
 console.log(numberLine);
 
-console.clear
 
-// const numberSeguence2 = '3, 4, 5, 6, 7, 8';
-let numberSeguence2 = start; 
+// Skaiciai intervale nuo 3 iki 8 imtinai yra: 3, 4, 5, 6, 7, 8.
 
-for(let i = start + 1; i <= finish; i++) {
-        numberSeguence2 += ', ' + i;
+// const numberSequence2 = '3, 4, 5, 6, 7, 8';
+let numberSequence2 = start;
+
+for (let i = start + 1; i <= finish; i++) {
+    numberSequence2 += ', ' + i;
 }
 
-const numberLine2 = `Skaiciai intervale nuo ${start} iki ${finish} imtinai yra:${numberSeguence2}.`;
+const numberLine2 = `Skaiciai intervale nuo ${start} iki ${finish} imtinai yra: ${numberSequence2}.`;
 
 console.log(numberLine2);
 
@@ -69,13 +69,6 @@ function arTestiCikla(index) {
 }
 
 for (let i = 0; arTestiCikla(i); i += 2) {
-       console.log(i, 'ciklas');
-
-i += 2;
-}
-console.log(i, 'uz ciklo');
-
-for (let i = 0; arTestiCikla(i); i +=2) {
     console.log(i, 'ciklas');
 }
 
@@ -83,39 +76,22 @@ console.clear();
 
 const word = 'zodelis';
 
-for(let i = 0; i < word.length; i++) {
-    console.log(i, `Raisde; ${word[i]}`);
+for (let i = 0; i < word.length; i++) {
+    console.log(i, `Raide: ${word[i]}`);
 }
 
-// Raide z;
-// Raide o;
-// Raide d;
-// Raide i;
-// Raide s;
-
-console.log(`raide'; ${word[0]}`);
-console.log(`raide'; ${word[1]}`);
-console.log(`raide'; ${word[2]}`);
-console.log(`raide'; ${word[3]}`);
-console.log(`raide'; ${word[4]}`);
-
 console.clear();
 
-for (let i = 0; i <= 4; i++); {
-    console.log (i, `Raide: ${word[i]}`);
-} 
-
-console.clear();
-
-//  0 1 2 3 4 5 
+//              0  1  2  3  4
 const marks = [10, 2, 8, 4, 6];
 
 let totalSum = 0;
 const amount = marks.length;
 
 for (let i = 0; i < amount; i++) {
-    totalSum += marks [i];
-    console.log(`marks[i], '=', totalSum`);
+    const mark = marks[i];
+    totalSum += mark;
+    console.log(`${i})`, mark, '=', totalSum);
 }
 
 const average = totalSum / amount;
--- Front-end/JS-intro/n-darbai/uzduotis.js
+++ Front-end/JS-intro/n-darbai/uzduotis.js
@@ -1,58 +1,59 @@
-// const svorisMark = 78; //kg
-// const svorisJohn = 95; //kg
+const svorisMark = 78; //kg
+const svorisJohn = 95; //kg
 
-// const ugisMark = 1.69; //m
-// const ugisJohn = 1.88; //m
+const ugisMark = 1.69; //m
+const ugisJohn = 1.88; //m
 
-// // KMI = masė / ūgis ** 2 = masė / (ūgis * ūgis) (masė kg ir aukštis metrais)
+// KMI = masė / ūgis ** 2 = masė / (ūgis * ūgis) (masė kg ir aukštis metrais)
 
-// const kmiMark = svorisMark / (ugisMark ** 2);
-// const kmiJohn = svorisJohn / (ugisJohn ** 2);
+const kmiMark = svorisMark / (ugisMark ** 2);
+const kmiJohn = svorisJohn / (ugisJohn ** 2);
 
-// console.log ('kmiMark', + kmiMark);
-// console.log('kmiJohn', + kmiJohn);
+console.log ('kmiMark', + kmiMark);
+console.log('kmiJohn', + kmiJohn);
 
-// const kmiPalyginimas = kmiMark > kmiJohn;
-// console.log('kmiPalyginimas/kmi Marko (27.31) didesnis nei kmi Johno (26,88)', kmiPalyginimas); 
+const kmiPalyginimas = kmiMark > kmiJohn;
+console.log('kmiPalyginimas/kmi Marko (27.31) didesnis nei kmi Johno (26,88)', kmiPalyginimas); 
 
-// const kmiPalyginimas2 = kmiMark < kmiJohn;
-// console.log('kmiPalyginimas/kmi Marko (27.31) mazesnis nei kmi Johno (26,88)', kmiPalyginimas2); 
-
-
-
-// // 2. Uzduotis su fukcija
-
-// const markoSvoris = 78; //kg
-// const johnoSvoris = 95; //kg
-
-// const markoUgis = 1.69; //m
-// const johnoUgis = 1.88; //m
-
-// const kmi = function kmi(ugis,svoris) {
-// return +(svoris/ (ugis * ugis)).toFixed(1);
-// }
-// const markoKMI1 = kmi(markoSvoris,markoUgis);
-// const  johnoKMI1 = kmi(johnoSvoris,johnoUgis);
-// console.log(typeof kmi(markoSvoris,markoUgis));
-// console.log(typeof kmi(johnoSvoris,johnoUgis));
-
-// if(markoKMI1 > johnoKMI1) {
-//    console.log(`"markHigherKMI' - true`);
-// }
-
-// if(markoKMI1 < johnoKMI1) {
-//     console.log (`"markHigherKMI" - false`);
-// }
+const kmiPalyginimas2 = kmiMark < kmiJohn;
+console.log('kmiPalyginimas/kmi Marko (27.31) mazesnis nei kmi Johno (26,88)', kmiPalyginimas2); 
 
 
 
+// 2. Uzduotis su fukcija
 
-const arr = [15, 15, 25, 3000, 25 ]
-const arr2 = [25, 45, 7, 18]
+const markoSvoris = 78; //kg
+const johnoSvoris = 95; //kg
+
+const markoUgis = 1.69; //m
+const johnoUgis = 1.88; //m
+
+const kmi = function kmi(ugis,svoris) {
+return +(svoris/ (ugis * ugis)).toFixed(1);
+}
+const markoKMI1 = kmi(markoSvoris,markoUgis);
+const  johnoKMI1 = kmi(johnoSvoris,johnoUgis);
+console.log(typeof kmi(markoSvoris,markoUgis));
+console.log(typeof kmi(johnoSvoris,johnoUgis));
+
+if(markoKMI1 > johnoKMI1) {
+console.log(`"markHigherKMI' - true`);
+}
+
+if(markoKMI1 < johnoKMI1) {
+console.log (`"markHigherKMI" - false`);
+}
 
 
-let sum = 0
+
+
+const arr = [1,1,1,1,1,1 ]
+const arr2 = [2,2,2,2,2]
+
+
+
 function avr(params) {
+    let sum = 0
     let avr = 0
     for (let i = 0; i < params.length; i++) {
        sum += params[i];
@@ -65,3 +66,4 @@ const dolphins = avr(arr)
 const koalas = avr(arr2)
 
 console.log(dolphins, koalas);
+
