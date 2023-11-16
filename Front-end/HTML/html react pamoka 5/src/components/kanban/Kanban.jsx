369a7b3ea4111b8923d414ac459b54f519cef817
VadimStarton <sedoigamer@gmail.com>
2023-11-16 13:29:36 +0200

    React Update

--- Front-end/HTML/html react pamoka 5/src/components/kanban/Kanban.jsx
+++ Front-end/HTML/html react pamoka 5/src/components/kanban/Kanban.jsx
@@ -1,49 +1,34 @@
+import { useState } from 'react';
 import style from './Kanban.module.css';
+import { KanbanColumn } from './KanbanColumn';
 
-export function Kanban() {
+export function Kanban({ tasks, removeTask }) {
+    const [title, setTitle] = useState('');
+    const [columns, setColumns] = useState(['Backlog', 'Todo']);
+
+    function newColumnSubmit(event) {
+        event.preventDefault();
+        setColumns(prev => [...prev, title]);
+        setTitle('');
+    }
+
+    function inputUpdate(event) {
+        setTitle(event.target.value);
+    }
+
     return (
-        <section id="kanban" className={style.todo} style={{ gridTemplateColumns: 'repeat(4, 1fr)' }}>
-            <div className={style.column}>
-                <h2 className={style.title + ' normal underline'}>Backlog</h2>
-                <ul className={style.taskList}>
-                    <li id="task_1" className={style.taskCard}>
-                        <div className={style.taskActions}>
-                            <button className={style.btn}>Delete</button>
-                            {/* <button className="fa fa-trash"></button> */}
-                        </div>
-                        <div className={style.taskTitle}>aaa</div>
-                        <div className={style.taskDesc}>aaa</div>
-                        <div className={style.taskTags}>
-                            <div className={style.tag} style={{ color: '#333' }}>aaa</div>
-                        </div>
-                        <div className={style.taskDeadline}>aaa</div>
-                    </li>
-                    <li id="task_2" className={style.taskCard}>
-                        <div className={style.taskActions}>
-                            <button className={style.btn}>Delete</button>
-                            {/* <button className="fa fa-trash"></button> */}
-                        </div>
-                        <div className={style.taskTitle}>bbb</div>
-                        <div className={style.taskDesc}>bbb</div>
-                        <div className={style.taskTags}>
-                            <div className={style.tag} style={{ color: '#333' }}>bbb</div>
-                        </div>
-                        <div className={style.taskDeadline}>bbb</div>
-                    </li>
-                </ul>
-            </div>
-            <div className={style.column}>
-                <h2 className={style.title + ' normal underline'}>Todo</h2>
-                <ul className={style.taskList}></ul>
-            </div>
-            <div className={style.column}>
-                <h2 className={style.title + ' normal underline'}>In progress</h2>
-                <ul className={style.taskList}></ul>
-            </div>
-            <div className={style.column}>
-                <h2 className={style.title + ' normal underline'}>Done</h2>
-                <ul className={style.taskList}></ul>
-            </div>
+        <section id="kanban" className={style.todo} style={{ gridTemplateColumns: `repeat(${columns.length + 1}, 1fr)` }}>
+            {columns.map((column, idx) => (
+                <KanbanColumn
+                    key={idx}
+                    tasks={tasks.filter(task => task.columnIndex === idx)}
+                    title={column}
+                    removeTask={removeTask} />
+            ))}
+            <form className={style.newColumn}>
+                <input type="text" value={title} onChange={inputUpdate} placeholder='Naujas stulpelis...' />
+                <button type="submit" onClick={newColumnSubmit}>Pridėti</button>
+            </form>
         </section>
     );
 }
