92097bca8650fb7ffb1e6ce3ad212621bb9bccba
VadimStarton <sedoigamer@gmail.com>
2023-12-29 09:18:23 +0200

    paskutine korekcija

--- Front-end/HTML/Workshop apps/indexmass/script.js
+++ Front-end/HTML/Workshop apps/indexmass/script.js
@@ -1,22 +1,34 @@
-const kmiText = document.getElementById('KMI');
-const duomenysText = document.getElementById('KMI');
+const kmiText = document.getElementById('kmi');
+const descText = document.getElementById('desc');
 const form = document.querySelector('form');
 
-form.addEventListener('submit', onformSubmit);
 
-function onformSubmit(e) {
+function onformbtn(e) {
     e.preventDefault();
-}
 
-const Svoris = parseFloat(form.input2.value);
-const Ūgis = parseFloat(form.input1.value);
+    const Ūgis = parseFloat(form.input1.value);
+    const Svoris = parseFloat(form.input2.value);
 
-
-if (isNaN(Svoris) || isNaN(Ūgis) || Svoris <= 0 || Ūgis <= 0) {
-   return ("Prašome įvesti skaičius");
+    if (isNaN(Svoris) || isNaN(Ūgis) || Svoris <= 0 || Ūgis <= 0) {
+    alert ("Prašome įvesti skaičius"); return;
     }
 
     const ŪgisMetrais = Ūgis/100;
-    const KMI = Svoris / Math.pow(ŪgisMetrais, 2);
+    const kmi = Svoris / Math.pow(ŪgisMetrais, 2);
+    const  desc = interpretkmi(kmi);
 
-    KMIText.textContent = KMI.toFixed(2);
+    kmiText.textContent = output.toFixed(2);
+    descText.innerHTML = 'Jus <strong>${desc}</strong>';
+};
+
+function outputDesc(kmi) {
+    if (kmi < 18.5) {
+        return 'Masės trūkumas';        
+    } else if (kmi < 25){
+        return 'Normalus';        
+    } else if (kmi < 30){
+        return 'Viršsvoris';
+    } else {
+        return 'Nutukimas';
+    }
+};
